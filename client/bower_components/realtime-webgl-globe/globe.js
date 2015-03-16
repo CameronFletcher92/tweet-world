d08ef4277947e97461d4c0791d942780b3dbd4f1
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-03-16 13:51:14 +1000

    using white glow

--- client/bower_components/realtime-webgl-globe/globe.js
+++ client/bower_components/realtime-webgl-globe/globe.js
@@ -155,7 +155,6 @@ var Globe = function(container, urls) {
           'varying vec3 vNormal;',
           'void main() {',
             'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 7.0 );',
-            //'gl_FragColor = vec4( 0.7, 1.0, 0.7, 1.0 ) * intensity;',
             'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
           '}'
         ].join('\n'),
