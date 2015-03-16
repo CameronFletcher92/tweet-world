1d78c4aff5b9a40e87f9f82d95a5d5862df58ded
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-03-16 21:17:11 +1000

    worked on the shaders

--- client/bower_components/realtime-webgl-globe/globe.js
+++ client/bower_components/realtime-webgl-globe/globe.js
@@ -148,7 +148,7 @@ var Globe = function(container, urls) {
         fragmentShader: [
           'varying vec3 vNormal;',
           'void main() {',
-            'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 7.0 );',
+            'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 5.0 );',
             'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
           '}'
         ].join('\n'),
